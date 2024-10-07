8d3c6dfba651b2e0571a14e099c001ddcefc1211
luchorossi222 <lucianorossi2003@gmail.com>
2024-10-07 23:28:47 +0000

    actualizacion final puntoc

--- parcial1/puntoc.sh
+++ parcial1/puntoc.sh
@@ -17,3 +17,8 @@ sudo whoami > /Examentes-UTN/alumno_1/validar.txt
 sudo whoami > /Examentes-UTN/alumno_2/validar.txt
 sudo whoami > /Examentes-UTN/alumno_3/validar.txt
 
+sudo shmod -r  750 /EXAMENES-UTN/alumno_1
+sudo shmod -r  760 /EXAMENES-UTN/alumno_2
+sudo shmod -r  700 /EXAMENES-UTN/alumno_3
+sudo shmod -r  775 /EXAMENES-UTN/profesores 
+
