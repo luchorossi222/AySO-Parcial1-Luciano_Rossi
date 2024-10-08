82441894b5f07324ebc222d8d9380aac10b1350d
luchorossi222 <lucianorossi2003@gmail.com>
2024-10-08 04:17:33 +0000

    push final

--- parcial1/puntof.sh
+++ parcial1/puntof.sh
@@ -1,6 +1,6 @@
 ARCHIVO_FILTRO_AVANZADO="Filtro_Avanzado.txt"
 
-cat << EOF |tee $ARCHIVO_FILTRO_AVANZADO
+cat << EOF | tee $ARCHIVO_FILTRO_AVANZADO
 Mi IP Publica es: $(curl -s ifconfig.me)
 Mi usuario es: $(whoami)
 El Hash de mi usuario es: $(sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}')
